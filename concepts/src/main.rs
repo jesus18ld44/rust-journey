e3fa868058d44f16ab15a0b99f6c10ec747ad4ef
Jesus <jarevalo80@alumno.uned.es>
2022-08-13 21:06:43 +0200

    .

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     let x = 5;
     println!("the value of x is: {x}");
+
+    // if expression
+    let number = 3;
+
+    if number < 5 {
+        println!("true");
+    } else {
+        println!("false");
+    }
+
+
+
 }

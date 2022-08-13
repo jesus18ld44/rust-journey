c2606aa9b979bfbd18b5bcec1fc9f568077fa113
Jesus <jarevalo80@alumno.uned.es>
2022-08-13 21:17:08 +0200

    loop labels

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -11,6 +11,25 @@ fn main() {
         println!("false");
     }
 
+    let mut cnt = 0;
+    'counting_up: loop {                    // loop labels must start with a single quote
+        println!("count = {cnt}");
+        let mut remaining = 10;
 
+        loop { 
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if cnt == 2 {
+                break 'counting_up;         // breaks the outer loop with the label
+            }
+            remaining -= 1;
+        }
+        
+        cnt += 1;
+    }
+
+    println!("end count = {cnt}");
 
 }
